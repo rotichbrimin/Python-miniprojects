fe9d43b20e07f924e8bb6f4df37e96f841c0ea62
rotichbrimin <rotichbrimin46@gmail.com>
2026-01-22 16:38:44 +0300

    upgrading voting system

--- ....py
+++ ....py
@@ -1,8 +1,35 @@
-age = int(input("Enter your age: "))
 
-if age < 18:
-    print("You are a minor.")
-elif age >= 18 and age < 59:
-    print("You are an adult.")
-else:
-    print("You are a senior citizen.")
+# nationality = 'Kenya'
+voted_count = 0
+total_people = 0
+minors = 0
+while True:
+    nationality = input("Enter your nationality :").lower()
+    age = int(input("Enter your age: "))
+    
+    if age < 0:
+        print("Age cannot be negative. Please enter a valid age.")
+        continue
+    if age >= 18 and nationality == 'kenya': 
+        print("You are kenyan adult and can vote")
+        voted_count += 1
+        total_people += 1
+    elif age < 18 and nationality == 'kenya':
+        print("You are a Kenyan minor and cannot vote!")
+        total_people += 1
+        minors += 1
+    elif age >= 18 and nationality != 'kenya':
+        print("Sorry! You are not a Kenyan and cannot vote !")
+        total_people += 1
+    else:
+        print("Sorry! You are not a Kenyan and a minor, so you cannot vote !")
+        total_people += 1
+        minors += 1
+
+    next_person = input("Is there another person to check? (yes/no): ").lower()
+    if next_person != 'yes':
+        break
+
+print(f"Total number of people checked are {total_people}")
+print(f"Total number of people who voted are {voted_count}")  
+print(f"Total number of minors are {minors}") 
--- multiplesum.py
+++ multiplesum.py
@@ -3,5 +3,5 @@ total = 0
 
 for i in range(1, n + 1):
     if i%3==0 or i%5==0:
-        total +=i
+        total +=i      
 print('Sum of numbers divisible by 3 or 5 is ', total)
